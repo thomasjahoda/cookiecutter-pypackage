2e824a3accc1d989469c741ed7e6480901dd5857
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-02-14 15:41:20 +0100

    fix path to dev-util

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
-        cli_bash_autocomplete_file = os.path.join('{{ cookiecutter.project_slug }}', 'dev-util', 'bash_autocomplete.sh')
+        cli_bash_autocomplete_file = os.path.join('dev-util', 'bash_autocomplete.sh')
         remove_file(cli_bash_autocomplete_file)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
