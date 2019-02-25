37334cf7f521f8c0128dddfce8a1b22ef4aaf34c
Thomas Jahoda <thomasjahoda@users.noreply.github.com>
2019-02-25 17:33:57 +0100

    install project in development mode initially using pip

--- {{cookiecutter.project_slug}}/dev-util/initial-setup.sh
+++ {{cookiecutter.project_slug}}/dev-util/initial-setup.sh
@@ -3,4 +3,9 @@
 echo "The virtualenv {{cookiecutter.project_slug}} is expected to have been created already via virtualenvwrapper"
 source activate-venv.sh || exit 1
 
-pip install --upgrade -r "../requirements/setup.txt" -r "../requirements/runtime.txt" -r "../requirements/test.txt"
+cd ..
+
+pip install --upgrade -r "requirements/setup.txt" -r "requirements/runtime.txt" -r "requirements/test.txt" || exit 1
+
+echo "Now installing project in development mode:"
+pip install -e . || exit 1
